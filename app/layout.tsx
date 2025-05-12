7a58572794a11a2c516ff08cecb49179b57de3a1
Dixit <72870029+DixitRam@users.noreply.github.com>
2025-05-12 20:22:29 +0000

    added more info in meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,38 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
-
 import { shadesOfPurple } from '@clerk/themes'
-
 import { Toaster } from 'sonner';
-
 import Navbar from "@/components/custom/navbar";
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
-
 export const metadata: Metadata = {
   title: "Portique",
   description: "Crafted Portfolios for Creative Minds.",
-openGraph: {
-      url: "https://portique.vercel.app",
-          siteName: "Portique",
-              images: '/og',
-                  locale: 'en_US',
-                      type: 'website',
-                        
-}
+  openGraph: {
+    url: "https://portique.vercel.app",
+    siteName: "Portique",
+    images: [
+      {
+        url: "https://portique.vercel.app/og",
+        width: 1200,
+        height: 630,
+        alt: "Portique - Launch Your Portfolio in Minutes!",
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+    title: "Portique",
+    description: "Crafted Portfolios for Creative Minds.",
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Portique",
+    description: "Crafted Portfolios for Creative Minds.",
+    images: ["https://portique.vercel.app/og"],
+    creator: "@portique",
+  }
 };
 
 export default function RootLayout({
@@ -32,11 +43,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
-appearance={{
-  baseTheme:shadesOfPurple,
-          variables: { colorBackground: '#00359e' },
-
-}}    >
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: { colorBackground: '#00359e' },
+      }}
+    >
       <html lang="en">
         <body className={`${poppins.className}  m-0 antialiased w-full h-screen`}>
           <Navbar/>
